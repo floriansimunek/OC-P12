614c4d87eec9235a634ba9503b6884cdc5875ae6
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-01 22:50:01 +0200

    :sparkles: Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+export default function Header() {
+	return (
+		<header>
+			<div className={styles.logo}>
+				<img src={logo} alt="Logo SportSee" />
+				<h1 className={styles.name}>SportSee</h1>
+			</div>
+			<ul className={styles.nav}>
+				<li className={styles.nav_item}>
+					<Link to="#" className={styles.nav_item_link}>
+						Accueil
+					</Link>
+				</li>
+				<li className={styles.nav_item}>
+					<Link to="#" className={styles.nav_item_link}>
+						Profil
+					</Link>
+				</li>
+				<li className={styles.nav_item}>
+					<Link to="#" className={styles.nav_item_link}>
+						Réglage
+					</Link>
+				</li>
+				<li className={styles.nav_item}>
+					<Link to="#" className={styles.nav_item_link}>
+						Communauté
+					</Link>
+				</li>
+			</ul>
+		</header>
+	);
+}
