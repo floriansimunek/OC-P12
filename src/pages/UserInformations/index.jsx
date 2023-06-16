059e2387ba60b939dcbe016700d456ec884262da
Florian SIMUNEK <florian.simunek@gmail.com>
2023-06-16 12:18:12 +0200

    :sparkles: Add CardsList with backend data

--- src/pages/UserInformations/index.jsx
+++ src/pages/UserInformations/index.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./UserInformations.module.scss";
 
 /* COMPONENTS */
 import Hello from "../../components/Hello";
+import CardsList from "../../components/CardsList";
 
 /* API */
 import Api from "../../utils/Api";
@@ -18,6 +20,7 @@ export default function UserInformations() {
 			try {
 				const userData = await API.getUserData(userID);
 				setUserData(userData);
+				console.log(userData);
 			} catch (error) {
 				console.error(error);
 			}
@@ -26,5 +29,47 @@ export default function UserInformations() {
 		fetchData();
 	}, [userID]);
 
-	return <div>{userData && <Hello userData={userData} />}</div>;
+	return (
+		<>
+			{userData && (
+				<div>
+					<Hello userData={userData} />
+					<div className={styles.datas}>
+						<CardsList
+							cards={[
+								{
+									type: "energy",
+									number: userData.keyData.calorieCount,
+									unity: "kCal",
+									name: "Calories",
+									color: "#ff0000",
+								},
+								{
+									type: "chicken",
+									number: userData.keyData.proteinCount,
+									unity: "g",
+									name: "Proteines",
+									color: "#4ab8ff",
+								},
+								{
+									type: "apple",
+									number: userData.keyData.carbohydrateCount,
+									unity: "g",
+									name: "Glucides",
+									color: "#fdcc0c",
+								},
+								{
+									type: "cheeseburger",
+									number: userData.keyData.lipidCount,
+									unity: "g",
+									name: "Lipides",
+									color: "#fd5181",
+								},
+							]}
+						/>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
