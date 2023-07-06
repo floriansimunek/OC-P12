c21cca3527ace14e4a350c5faa600c16614e0aea
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-06 15:06:21 +0200

    :recycle: Refactor MockedService path switch

--- src/services/MockedService.js
+++ src/services/MockedService.js
@@ -2,9 +2,9 @@ import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE 
 
 export default class MockedService {
 	static async fetchData(path) {
-		const endPath = path.split("/");
+		const endPath = path.match(/\/\d+\/(?<path>[a-zA-Z-]+)/)?.groups.path;
 
-		switch (endPath[2]) {
+		switch (endPath) {
 			case "activity":
 				return USER_ACTIVITY;
 			case "average-sessions":
