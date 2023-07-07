94daa1e26c11e1be2523e7d767c4d7c674b7b197
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-07 14:36:17 +0200

    :memo: Add JSDoc for MenuItem component

--- src/components/MenuItem/index.jsx
+++ src/components/MenuItem/index.jsx
@@ -1,6 +1,11 @@
 import styles from "./MenuItem.module.scss";
 import { Link } from "react-router-dom";
 
+/**
+ *
+ * @param {String} icon Icon src location
+ * @returns {JSX.Element} Menu Item Link Element
+ */
 export default function MenuItem({ icon }) {
 	return (
 		<Link to="#" className={styles.nav_item_link}>
