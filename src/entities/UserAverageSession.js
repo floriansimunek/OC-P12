8513c48c970b7f889193ba873e071d78aeb06b57
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-11 17:51:38 +0200

    :memo: Add JSDoc for UserAverageSession entity

--- src/entities/UserAverageSession.js
+++ src/entities/UserAverageSession.js
@@ -1,9 +1,16 @@
+/**
+ * Represents a user average session
+ */
 export default class UserAverageSession {
 	day;
 	dayOfWeek;
 	sessionLength;
 	userId;
 
+	/**
+	 *
+	 * @param {object} averageSessionData user average session datas
+	 */
 	constructor(averageSessionData) {
 		this.day = averageSessionData.day;
 		this.dayOfWeek = averageSessionData.dayOfWeek;
