8974412394e21a80372852fe93d16693480ab8d7
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-11 17:54:24 +0200

    :memo: Add JSDoc for UserAverageSessionsCollection entity

--- src/entities/UserAverageSessionsCollection.js
+++ src/entities/UserAverageSessionsCollection.js
@@ -1,14 +1,26 @@
 import UserAverageSession from "./UserAverageSession";
 
+/**
+ * Collection of UserAverageSession
+ */
 export default class UserAverageSessionsCollection {
 	averageSessions = [];
 	userId = null;
 
+	/**
+	 *
+	 * @param {object} averageSessionData user average session data
+	 */
 	constructor(averageSessionData) {
 		this.userId = averageSessionData.userId;
 		this.averageSessions = this.parseData(averageSessionData);
 	}
 
+	/**
+	 *
+	 * @param {object} data get user average sessions data
+	 * @returns {object} parsedData of the user average sessions data
+	 */
 	parseData(data) {
 		const daysOfWeek = ["L", "M", "M", "J", "V", "S", "D"];
 		const parsedData = data.sessions.map((session, k) => new UserAverageSession({ ...session, dayOfWeek: daysOfWeek[k], userId: this.userId }));
