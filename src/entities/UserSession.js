5bb1b0139fe576f48b1fa21e69427bb2762caf28
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-11 18:01:39 +0200

    :memo: Add JSDoc for UserSession entity

--- src/entities/UserSession.js
+++ src/entities/UserSession.js
@@ -1,9 +1,16 @@
+/**
+ * Represents a user session
+ */
 export default class UserSession {
 	day;
 	kilogram;
 	calories;
 	userId;
 
+	/**
+	 *
+	 * @param {object} sessionData user session data
+	 */
 	constructor(sessionData) {
 		this.day = new Date(sessionData.day);
 		this.kilogram = sessionData.kilogram;
