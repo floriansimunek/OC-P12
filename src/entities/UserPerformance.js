25fdd56fa9774f2b19962daddb9d0fccd75f5e34
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-11 17:57:06 +0200

    :memo: Add JSDoc for UserPerformance entity

--- src/entities/UserPerformance.js
+++ src/entities/UserPerformance.js
@@ -1,8 +1,15 @@
+/**
+ * Represents a user performance
+ */
 export default class UserPerformance {
 	kind;
 	value;
 	userId;
 
+	/**
+	 *
+	 * @param {object} performancesData user performance data
+	 */
 	constructor(performancesData) {
 		this.kind = performancesData.kind;
 		this.value = performancesData.value;
