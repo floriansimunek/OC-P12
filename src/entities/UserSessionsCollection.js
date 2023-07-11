f33f076f9591f64211e12cea47a17d4ac827e6a2
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-11 18:02:27 +0200

    :memo: Add JSDoc for UserSessionsCollection entity

--- src/entities/UserSessionsCollection.js
+++ src/entities/UserSessionsCollection.js
@@ -1,9 +1,16 @@
 import UserSession from "./UserSession";
 
+/**
+ * Collection of UserSession
+ */
 export default class UserSessionsCollection {
 	sessions = [];
 	userId = null;
 
+	/**
+	 *
+	 * @param {object} sessionsData user sessions data
+	 */
 	constructor(sessionsData) {
 		this.userId = sessionsData.userId;
 		this.sessions = sessionsData.sessions.map((session) => new UserSession({ ...session, userId: this.userId }));
